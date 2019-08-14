e6d7a27d6de93ccf9c53b863ee379a1aea5bed49
mimimi32k <comame.mizuki@gmail.com>
2019-08-14 21:25:37 +0900

    message.js2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,3 +9,35 @@ $(function() {
                      ${message.created_at}
                    </div>
                   </div>
+                  <div class="main_contents__body__timeline__message">
+                    <p class="main_contents__body__timeline__message__content">
+                      ${message.content}
+                    <img class="lower-message__image" src="/message/image/${message.image}">
+                    </p>
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = window.location.href + '/comments'
+                    $.ajax({
+                      url: href,
+                      type: "POST",
+                      data: formData,
+                      dataType: 'json',
+                      processData: false,
+                      contentType: false
+                    })
+                    .done(function(message){
+                      var html = buildHTML(message);
+                      $('.comments').append(html)
+                      $('.textbox').val('')
+                    })
+                  })
+                });
+
+
+                  
