644177e8a4b583b01a133596c1e81a0ef996818a
mimimi32k <comame.mizuki@gmail.com>
2019-08-18 12:03:01 +0900

    edit_group_user

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,22 +1,28 @@
 $(function(){
   function buildHTML(user){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    var html = `<div class='chat-group-user clearfix'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                <div class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
-  $("#user-search-result").append(html);
+  $('#user-search-result').append(html);
+  }
+  function buildUser(userId, userName){
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                  <p class='chat-group-user__name'>${userName}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+  $('.chat-group-form__field--right#user_list').append(html);
   }
   
   $('#user-search-field').on('keyup',function(){
     var input = $('#user-search-field').val();
-
     $.ajax({
       type: 'GET',
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
-
    .done(function(users){
      $('#user-search-result').empty();
       users.forEach(function(user){ 
@@ -28,12 +34,13 @@ $(function(){
    })
   });
   $('#user-search-result').on('click', '.user-search-add', function(){
-    var userId = $(this).data('取得したid');
-    var userName = $(this).data('取得したname');
-    htmlを呼ぶ関数名(設定した２つの変数を引数として代入);
     $(this).parent().remove();
-    console.log("OK");
-   });
-  
+    var userId = $(this).attr('data-user-id');
+    var userName = $(this).attr('data-user-name');
+    buildUser(userId, userName);
+  });
+  $('.chat-group-user').on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
+  });
    
 });
